e660c523a9cf52887b32218df199d71d56e09f22
pranoti <pranotikadam96@gmail.com>
2024-07-05 16:06:57 +0530

    responsiveness added

--- frontend/src/components/DataTable.jsx
+++ frontend/src/components/DataTable.jsx
@@ -47,66 +47,68 @@ const DataTable = ({ role, data, setData }) => {
     return (
         <div className="datatable-container">
             <h2>Data Table</h2>
-            <table className="data-table">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Quantity</th>
-                        <th>Amount</th>
-                        <th>Posting Year</th>
-                        <th>Posting Month</th>
-                        <th>Action Name</th>
-                        <th>Action Type</th>
-                        <th>Action Number</th>
-                        <th>Impact</th>
-                        <th>Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {editedData.map(row => (
-                        <tr key={row.id}>
-                            <td>{row.id}</td>
-                            <td>{row.quantity}</td>
-                            <td>
-                                {editMode ? (
-                                    <input type="number" value={row.amount} onChange={e => handleEdit(row.id, 'amount', e.target.value)} />
-                                ) : (row.amount)}
-                            </td>
-                            <td>{row.postingYear}</td>
-                            <td>{row.postingMonth}</td>
-                            <td>
-                                {editMode ? (
-                                    <select value={row.actionName} onChange={e => handleEdit(row.id, 'actionName', e.target.value)} >
-                                        <option value="Action 1">Action 1</option>
-                                        <option value="Action 2">Action 2</option>
-                                        <option value="Action 3">Action 3</option>
-                                    </select>
-                                ) : (row.actionName)}
-                            </td>
-                            <td>
-                                {editMode ? (
-                                    <select value={row.actionType} onChange={e => handleEdit(row.id, 'actionType', e.target.value)} >
-                                        <option value="Type 1">Type 1</option>
-                                        <option value="Type 2">Type 2</option>
-                                        <option value="Type 3">Type 3</option>
-                                    </select>
-                                ) : (row.actionType)}
-                            </td>
-                            <td>{row.actionNumber}</td>
-                            <td>{row.Impact}</td>
-                            <td>
-                                {editMode && role === 'admin' ? (
-                                    <select value={row.status} onChange={e => handleEdit(row.id, 'status', e.target.value)}>
-                                        <option value="Pending">Pending</option>
-                                        <option value="In Progress">In Progress</option>
-                                        <option value="Approved">Approved</option>
-                                    </select>
-                                ) : (row.status)}
-                            </td>
+            <div className='table-wrapper'>
+                <table className="data-table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Quantity</th>
+                            <th>Amount</th>
+                            <th>Posting Year</th>
+                            <th>Posting Month</th>
+                            <th>Action Name</th>
+                            <th>Action Type</th>
+                            <th>Action Number</th>
+                            <th>Impact</th>
+                            <th>Status</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {editedData.map(row => (
+                            <tr key={row.id}>
+                                <td>{row.id}</td>
+                                <td>{row.quantity}</td>
+                                <td>
+                                    {editMode ? (
+                                        <input type="number" value={row.amount} onChange={e => handleEdit(row.id, 'amount', e.target.value)} />
+                                    ) : (row.amount)}
+                                </td>
+                                <td>{row.postingYear}</td>
+                                <td>{row.postingMonth}</td>
+                                <td>
+                                    {editMode ? (
+                                        <select value={row.actionName} onChange={e => handleEdit(row.id, 'actionName', e.target.value)} >
+                                            <option value="Action 1">Action 1</option>
+                                            <option value="Action 2">Action 2</option>
+                                            <option value="Action 3">Action 3</option>
+                                        </select>
+                                    ) : (row.actionName)}
+                                </td>
+                                <td>
+                                    {editMode ? (
+                                        <select value={row.actionType} onChange={e => handleEdit(row.id, 'actionType', e.target.value)} >
+                                            <option value="Type 1">Type 1</option>
+                                            <option value="Type 2">Type 2</option>
+                                            <option value="Type 3">Type 3</option>
+                                        </select>
+                                    ) : (row.actionType)}
+                                </td>
+                                <td>{row.actionNumber}</td>
+                                <td>{row.Impact}</td>
+                                <td>
+                                    {editMode && role === 'admin' ? (
+                                        <select value={row.status} onChange={e => handleEdit(row.id, 'status', e.target.value)}>
+                                            <option value="Pending">Pending</option>
+                                            <option value="In Progress">In Progress</option>
+                                            <option value="Approved">Approved</option>
+                                        </select>
+                                    ) : (row.status)}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
             <div className="data-table-actions">
                 <button onClick={() => setEditMode(!editMode)}>
                     {editMode ? 'Cancel' : 'Edit'}
